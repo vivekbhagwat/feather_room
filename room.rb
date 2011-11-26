8fb78c7aae4c68328bc8e96f47ef6489a78a2be7
Vivek Bhagwat <bhagwat.vivek@gmail.com>
2011-11-26 13:12:55 -0800

    Added upload page

--- room.rb
+++ room.rb
@@ -1,9 +1,18 @@
 require 'sinatra'
 
 get '/' do
-  
+  @title = "Feather Room"
+  @footer = "Created by Vivek Bhagwat"
+  erb :index
 end
 
 get '/hi' do
   "Hello, World!"
+end
+
+get '/upload/?' do
+  @title = "Feather Room - Upload Photos"
+  @body = '<h1>Upload Photos</h1>'
+  @body += '<h2>Enter a bunch of URLs of photos to "upload"</h2>'
+  @body += '<textarea rows="4" cols="50" />'
 end
