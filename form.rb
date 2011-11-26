959f46da3f6ab882c65bbf37223801771875f704
Vivek Bhagwat <bhagwat.vivek@gmail.com>
2011-11-26 13:16:17 -0800

    Added Form class

---
+++ form.rb
@@ -0,0 +1,37 @@
+class Form
+  attr_accessor :action, :method, :content, :submit
+  
+  def initialize(action='/',method='GET',content=[],submit={})
+    @action = action
+    @method = method
+    @content = content
+    @submit = submit
+  end
+  
+  
+  # '<form action="@action" method="@method">
+  # <input type="type1" value="value1" .. />
+  # ...
+  # <input type="submit" value="submit_value" ... />
+  # </form>'
+  def to_s
+    str = '<form action="' + @action + '" method="' + @method + '">'
+    @content.each do |c|
+      if c.is_a?Hash
+        str += c[:content].to_s + '<input '
+        (c.keys - [:content]).each do |input|
+          str += input.to_s + '="' + c[input].to_s + '" '
+        end
+        str += " />\n"
+      end
+    end
+    
+    str += '<input type="submit" '
+    @submit.each do |k,v|
+      str += k.to_s + '="' + v.to_s + '" '
+    end
+    str += '/>'
+    
+    str + '</form>'
+  end
+end
