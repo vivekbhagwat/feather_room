132b61402dec559cbbd676ffdfd777859d1e3188
Vivek Bhagwat <bhagwat.vivek@gmail.com>
2011-11-26 16:50:30 -0800

    defined photo class

--- photo.rb
+++ photo.rb
@@ -1,2 +1,12 @@
+require 'open-uri'
 class Photo
+  attr_accessor :filename, :data, :id
+  
+  def initialize(filename='')
+    @filename = filename
+    
+    @data = open(filename.to_s) {|f| f.read}
+    @id = nil
+  end
+  
 end
