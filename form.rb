e8c42b006814f4c75068893ddabcfc8f5af87a5d
Vivek Bhagwat <bhagwat.vivek@gmail.com>
2011-11-27 18:40:57 -0800

    Added support for file upload

--- form.rb
+++ form.rb
@@ -1,11 +1,12 @@
 class Form
-  attr_accessor :action, :method, :content, :submit
+  attr_accessor :action, :method, :content, :submit, :is_file
   
-  def initialize(action='/',method='GET',content=[],submit={})
+  def initialize(action='/',method='GET',content=[],submit={}, file=false)
     @action = action
     @method = method
     @content = content
     @submit = submit
+    @is_file = file
   end
   
   
@@ -15,7 +16,11 @@ class Form
   # <input type="submit" value="submit_value" ... />
   # </form>'
   def to_html
-    str = '<form action="' + @action + '" method="' + @method + '">'
+    enctype = 'enctype="multipart/form-data"' if @is_file
+    enctype ||= ''
+    
+    str = '<form action="' + @action + '" method="' + @method + '" ' + enctype + '>'
+    
     @content.each do |c|
       if c.is_a?Hash
         str += c[:content].to_s + '<input '
@@ -23,6 +28,8 @@ class Form
           str += input.to_s + '="' + c[input].to_s + '" '
         end
         str += " />\n"
+      elsif c.nil?
+        str += "<br />\n"
       end
     end
     
